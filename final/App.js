efb1d6a3a9982e7d0f00419f3d0e2b067124cf11
a_nedaseikin <andrei.nedaseikin@gmail.com>
2018-01-06 18:44:17 +0300

    redux with middleware are plugged in

--- final/App.js
+++ final/App.js
@@ -4,16 +4,18 @@ require('es6-promise').polyfill();
 
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import { createStore }   from 'redux';
-import { Provider }      from 'react-redux';
-import combinedReducer   from './reducers'
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import combinedReducer from './reducers'
 
 import PagesRouter from './pages/PagesRouter';
 import PagesLinks  from './pages/PagesLinks';
 
 import './App.scss';
 
-let store = createStore( combinedReducer );
+let store = createStore( combinedReducer, composeWithDevTools( applyMiddleware( thunk ) ) );
 
 class App extends React.PureComponent {
 
@@ -22,12 +24,15 @@ class App extends React.PureComponent {
     render() {
         let state = store.getState();
         return (
-            <BrowserRouter>
-                <div className = { this.classCSS }>
-                    <PagesLinks />
-                    <PagesRouter />
-                </div>
-            </BrowserRouter>
+            <Provider store = { store }>
+                <BrowserRouter>
+                    <div className = { this.classCSS }>
+                        <PagesLinks />
+                        <PagesRouter />
+                    </div>
+                </BrowserRouter>
+            </Provider>
+
         )
     }
 }
